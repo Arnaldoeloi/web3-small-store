f13cfa92eff0af365a24bb0dcadaf4a2343fa0df
Arnaldo Souza <arnaldoeloi2@gmail.com>
2022-12-18 22:19:35 -0300

    Update contractAbi.js

--- src/util/contractAbi.js
+++ src/util/contractAbi.js
@@ -1,4 +1,149 @@
-const abi = [
+[
+	{
+		"inputs": [
+			{
+				"internalType": "uint256[]",
+				"name": "_products",
+				"type": "uint256[]"
+			},
+			{
+				"internalType": "uint8[]",
+				"name": "_amounts",
+				"type": "uint8[]"
+			}
+		],
+		"name": "buyProducts",
+		"outputs": [],
+		"stateMutability": "payable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_productId",
+				"type": "uint256"
+			},
+			{
+				"internalType": "string",
+				"name": "name",
+				"type": "string"
+			},
+			{
+				"internalType": "uint256",
+				"name": "price",
+				"type": "uint256"
+			},
+			{
+				"internalType": "string",
+				"name": "img_link",
+				"type": "string"
+			},
+			{
+				"internalType": "uint8",
+				"name": "stock",
+				"type": "uint8"
+			},
+			{
+				"internalType": "bool",
+				"name": "_canBeBought",
+				"type": "bool"
+			}
+		],
+		"name": "editProduct",
+		"outputs": [
+			{
+				"components": [
+					{
+						"internalType": "uint256",
+						"name": "id",
+						"type": "uint256"
+					},
+					{
+						"internalType": "string",
+						"name": "name",
+						"type": "string"
+					},
+					{
+						"internalType": "uint256",
+						"name": "price",
+						"type": "uint256"
+					},
+					{
+						"internalType": "string",
+						"name": "img_link",
+						"type": "string"
+					},
+					{
+						"internalType": "uint8",
+						"name": "stock",
+						"type": "uint8"
+					},
+					{
+						"internalType": "bool",
+						"name": "canBeBought",
+						"type": "bool"
+					}
+				],
+				"internalType": "struct SmallStore.Product",
+				"name": "",
+				"type": "tuple"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_productId",
+				"type": "uint256"
+			}
+		],
+		"name": "removeProduct",
+		"outputs": [
+			{
+				"components": [
+					{
+						"internalType": "uint256",
+						"name": "id",
+						"type": "uint256"
+					},
+					{
+						"internalType": "string",
+						"name": "name",
+						"type": "string"
+					},
+					{
+						"internalType": "uint256",
+						"name": "price",
+						"type": "uint256"
+					},
+					{
+						"internalType": "string",
+						"name": "img_link",
+						"type": "string"
+					},
+					{
+						"internalType": "uint8",
+						"name": "stock",
+						"type": "uint8"
+					},
+					{
+						"internalType": "bool",
+						"name": "canBeBought",
+						"type": "bool"
+					}
+				],
+				"internalType": "struct SmallStore.Product",
+				"name": "",
+				"type": "tuple"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
 	{
 		"inputs": [],
 		"stateMutability": "nonpayable",
@@ -99,29 +244,6 @@ const abi = [
 	{
 		"inputs": [
 			{
-				"internalType": "uint256[]",
-				"name": "_products",
-				"type": "uint256[]"
-			},
-			{
-				"internalType": "uint8[]",
-				"name": "_amounts",
-				"type": "uint8[]"
-			}
-		],
-		"name": "buyProducts",
-		"outputs": [],
-		"stateMutability": "payable",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "uint256",
-				"name": "_productId",
-				"type": "uint256"
-			},
-			{
 				"internalType": "string",
 				"name": "name",
 				"type": "string"
@@ -140,53 +262,17 @@ const abi = [
 				"internalType": "uint8",
 				"name": "stock",
 				"type": "uint8"
-			},
-			{
-				"internalType": "bool",
-				"name": "_canBeBought",
-				"type": "bool"
 			}
 		],
-		"name": "editProduct",
-		"outputs": [
-			{
-				"components": [
-					{
-						"internalType": "uint256",
-						"name": "id",
-						"type": "uint256"
-					},
-					{
-						"internalType": "string",
-						"name": "name",
-						"type": "string"
-					},
-					{
-						"internalType": "uint256",
-						"name": "price",
-						"type": "uint256"
-					},
-					{
-						"internalType": "string",
-						"name": "img_link",
-						"type": "string"
-					},
-					{
-						"internalType": "uint8",
-						"name": "stock",
-						"type": "uint8"
-					},
-					{
-						"internalType": "bool",
-						"name": "canBeBought",
-						"type": "bool"
-					}
-				],
-				"internalType": "struct SmallStore.Product",
-				"name": "",
-				"type": "tuple"
-			}
-		],
+		"name": "storeProduct",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "withdraw",
+		"outputs": [],
 		"stateMutability": "nonpayable",
 		"type": "function"
 	},
@@ -370,92 +456,5 @@ const abi = [
 		],
 		"stateMutability": "view",
 		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "uint256",
-				"name": "_productId",
-				"type": "uint256"
-			}
-		],
-		"name": "removeProduct",
-		"outputs": [
-			{
-				"components": [
-					{
-						"internalType": "uint256",
-						"name": "id",
-						"type": "uint256"
-					},
-					{
-						"internalType": "string",
-						"name": "name",
-						"type": "string"
-					},
-					{
-						"internalType": "uint256",
-						"name": "price",
-						"type": "uint256"
-					},
-					{
-						"internalType": "string",
-						"name": "img_link",
-						"type": "string"
-					},
-					{
-						"internalType": "uint8",
-						"name": "stock",
-						"type": "uint8"
-					},
-					{
-						"internalType": "bool",
-						"name": "canBeBought",
-						"type": "bool"
-					}
-				],
-				"internalType": "struct SmallStore.Product",
-				"name": "",
-				"type": "tuple"
-			}
-		],
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "string",
-				"name": "name",
-				"type": "string"
-			},
-			{
-				"internalType": "uint256",
-				"name": "price",
-				"type": "uint256"
-			},
-			{
-				"internalType": "string",
-				"name": "img_link",
-				"type": "string"
-			},
-			{
-				"internalType": "uint8",
-				"name": "stock",
-				"type": "uint8"
-			}
-		],
-		"name": "storeProduct",
-		"outputs": [],
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"inputs": [],
-		"name": "withdraw",
-		"outputs": [],
-		"stateMutability": "nonpayable",
-		"type": "function"
 	}
 ]
-export default abi;
